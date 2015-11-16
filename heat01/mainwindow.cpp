c125ed2c574f759accc0fa6ec98948831d57d236
catof21 <catof21@gmail.com>
2015-11-16 08:49:02 +0100

    plot + calc

--- heat01/mainwindow.cpp
+++ heat01/mainwindow.cpp
@@ -17,12 +17,15 @@ MainWindow::~MainWindow()
 void MainWindow::makePlot()
 {
     // generate some data:
-    QVector<double> x(101), y(101); // initialize with entries 0..100
+    QVector<double> x(200), y(6500); // initialize with entries 0..100
+
+
     for (int i=0; i<101; ++i)
     {
       x[i] = i/50.0 - 1; // x goes from -1 to 1
       y[i] = x[i]*x[i]; // let's plot a quadratic function
     }
+
     // create graph and assign data to it:
     ui->plot->addGraph();
     ui->plot->graph(0)->setData(x, y);
